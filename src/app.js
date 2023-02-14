7547799e5085bcde613bb9c9bedb0ab8042b5c58
CaolanCode <cmacmahon3@gmail.com>
2023-02-14 20:28:47 +0000

    commit background image

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import header from './header'
 import './styles/style.css'
+import background from './background.js';
 
 const container = document.createElement('div')
 container.classList.add('container')
 
 container.appendChild(header())
+container.appendChild(background())
 document.body.appendChild(container)
